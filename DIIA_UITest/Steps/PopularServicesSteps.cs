cb408b78de106aaa860d32f9899423ce04dd45bc
Irina-Koida <91370760+Irina-Koida@users.noreply.github.com>
2021-11-22 12:42:29 +0200

    // popular servises steps

--- DIIA_UITest/Steps/PopularServicesSteps.cs
+++ DIIA_UITest/Steps/PopularServicesSteps.cs
@@ -7,73 +7,74 @@ namespace DIIA_UITest.Steps
     [Binding]
     public class PopularServicesSteps
     {
+
         private readonly ScenarioContext _scenarioContext;
         private readonly PopularServisesFromMainMenu _popularServisesFromMainMenu;
         private readonly IWebDriver _webDriver;
 
-        public PopularServicesSteps(ScenarioContext scenarioContext)
+        public PopularServicesSteps (ScenarioContext scenarioContext)
         {
             _scenarioContext = scenarioContext;
             _webDriver = _scenarioContext.Get<IWebDriver>("WebDriver");
-            _popularServisesFromMainMenu = new PopularServisesFromMainMenu(_webDriver);
+            _popularServisesFromMainMenu = new PopularServisesFromMainMenu (_webDriver);
         }
-
+        
         [When(@"I click on the first service")]
         public void WhenIClickOnTheFirstService()
         {
             _popularServisesFromMainMenu.GoToPopularServicePage();
         }
-
+        
         [When(@"I click on the swiper rigt button")]
         public void WhenIClickOnTheSwiperRigtButton()
         {
-            _popularServisesFromMainMenu.SwipeToTheRightClick();
+            _popularServisesFromMainMenu.ClickToTheRightSide();
         }
-
+        
         [When(@"I click on the swiper left button")]
         public void WhenIClickOnTheSwiperLeftButton()
         {
-            _popularServisesFromMainMenu.SwipeToTheLeftClick();
+            _popularServisesFromMainMenu.ClickToTheLeftSide();
         }
-
+        
         [When(@"I click on the right side of the pagination bullet bar")]
         public void WhenIClickOnTheRightSideOfThePaginationBulletBar()
         {
-            _popularServisesFromMainMenu.ClickToTheRightSide();
+            _popularServisesFromMainMenu.SwipeToTheRightClick();
         }
-
-        [When(@"I click on the left side of the pagination bullet bar")]
-        public void WhenIClickOnTheLeftSideOfThePaginationBulletBar()
+        
+        [When(@"I click from left to right on the pagination bullet bar")]
+        public void WhenIClickFromLeftToRightOnThePaginationBulletBar()
         {
-            _popularServisesFromMainMenu.ClickToTheLeftSide();
+            _popularServisesFromMainMenu.SwipeToTheLeftClick();
         }
-
+        
         [Then(@"I navigate to the service page")]
         public void ThenINavigateToTheServicePage()
         {
             _popularServisesFromMainMenu.GoToPopularServicePage();
         }
-
+        
         [Then(@"Popular servises moved right on one position")]
         public void ThenPopularServisesMovedRightOnOnePosition()
         {
             _popularServisesFromMainMenu.MovedToTheRight();
         }
-
+        
         [Then(@"Popular servises moved left on one position")]
         public void ThenPopularServisesMovedLeftOnOnePosition()
         {
             _popularServisesFromMainMenu.MovedToTheLeft();
         }
-
-        [Then(@"Popular services moved right on one position\.")]
-        public void ThenPopularServicesMovedRightOnOnePosition_()
+        
+        [Then(@"Popular services moved one position\.")]
+        public void ThenPopularServicesMovedOnePosition_()
         {
             _popularServisesFromMainMenu.MovedToTheRight();
         }
-
-        [Then(@"Popular services moved left on one position\.")]
-        public void ThenPopularServicesMovedLeftOnOnePosition_()
+        
+        [Then(@"Popular services moved one position to the left\.")]
+        public void ThenPopularServicesMovedOnePositionToTheLeft_()
         {
             _popularServisesFromMainMenu.MovedToTheLeft();
         }
